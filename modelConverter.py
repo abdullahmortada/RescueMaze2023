1beabb5df55a9e4fa6895436b33545083ba8be02
Abdullah Mortada <abdullahmortada@gmail.com>
2023-02-05 14:23:52 +0200

    scripts for pi

---
+++ modelConverter.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+savedPath = "./training/cp-0008.ckpt"
+toSave = "./lite/liteModel.tflite"
+
+converter = tf.lite.TFLiteConverter.from_saved_model(savedPath)
+tflite_model = converter.convert()
+with open(toSave, 'wb') as f:
+    f.write(tflite_model)
