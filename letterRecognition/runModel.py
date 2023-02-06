077850be05146bd9ead4da07234503e81c20cb64
Abdullah Mortada <abdullahmortada@gmail.com>
2023-02-06 10:40:50 +0200

    made for pi

--- letterRecognition/runModel.py
+++ letterRecognition/runModel.py
@@ -1,19 +1,19 @@
-import numpy
-import tensorflow as tf 
+import numpy as np
+import tflite_runtime.interpreter as tflite
 import cv2 
 import findLetters
 
 def filter(img):
-    img = tf.convert_to_tensor(img)
-    img = tf.image.convert_image_dtype(img, tf.float32)
-    # img = tf.transpose(img, perm=[1, 0, 2])
-    img = tf.reshape(img, (1, 32, 32,1))
+    img = cv2.resize(img, (32, 32))
+    img = cv2.transpose(img)
+    img = img.astype(np.float32)
+    img = img.reshape((1, 32, 32, 1))
     return img
 
 model_path = "./lite/liteModel.tflite"
 
 def runModel(img):
-    interpreter = tf.lite.Interpreter(model_path)
+    interpreter = tflite.Interpreter(model_path)
     interpreter.allocate_tensors()
 
     input_details = interpreter.get_input_details()
@@ -23,8 +23,7 @@ def runModel(img):
     interpreter.set_tensor(input_details[0]['index'], img)
     interpreter.invoke()
     preds = interpreter.get_tensor(output_details[0]['index'])
-    predNums = numpy.argmax(preds, axis=-1)
-    print(preds)
+    predNums = np.argmax(preds, axis=-1)
     return predNums
 
 img = cv2.imread("/home/abdullah/Downloads/H2.jpg", cv2.IMREAD_GRAYSCALE)
